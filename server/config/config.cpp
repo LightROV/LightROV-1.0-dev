afa14ea9fe136069445b562c8b342d99fc7b1a9f
itsl <itsl@foxmail.com>
2018-03-09 17:56:02 +0800

    确定了指令规则

--- server/config/config.cpp
+++ server/config/config.cpp
@@ -1,12 +1,23 @@
-//
-// Created by itsl on 18-3-8.
-//
+/**
+ * 这里是定义标识符的文件
+ * 编译时本文件生成被编译成静态链接库 libconfig.a
+ *
+ * Created by itsl on 18-3-8.
+ */
 
 #include "config.hpp"
 
 using ltrov::config::Event;
 
-typedef unsigned int uint;
+// 标识位占8位长度
+typedef unsigned char uchar;
 
-uint Event::REQUEST_VIDEO_OPEN  = 0x00000001;
-uint Event::REQUEST_VIDEO_CLOSE = 0x00000002;
+uchar Event::REQUEST_VIDEO_OPEN     = 0x01;
+uchar Event::REQUEST_VIDEO_CLOSE    = 0x02;
+
+uchar Event::MOTION_FORWARD         = 0x10;
+uchar Event::MOTION_BACK            = 0x11;
+uchar Event::MOTION_UP              = 0x12;
+uchar Event::MOTION_DOWN            = 0x13;
+uchar Event::MOTION_ROUND           = 0x14;
+uchar Event::CONFIG_SPEED           = 0x15;
--- server/config/config.hpp
+++ server/config/config.hpp
@@ -1,6 +1,13 @@
-//
-// Created by itsl on 18-3-8.
-//
+/**
+ * Created by itsl on 18-3-8.
+ *
+ * #说明#
+ * 1. 每个控制指令为32位长
+ * 2. 结束标志占4位，同时作为高28位的摘要，摘要计算方法：flag=min(#1, #0)
+ * 3. 前8位为标识位，用以区分指令
+ * 4. 中间20位(9-28)为数据位，根据不同情况代表意义不同
+ *
+ */
 
 #ifndef LIGHTRO_1_0_SERVER_CONFIG_HPP
 #define LIGHTRO_1_0_SERVER_CONFIG_HPP
@@ -8,12 +15,28 @@
 namespace ltrov{
 namespace config{
 
-typedef unsigned int uint;
+typedef unsigned char uchar;
 
 class Event {
 public:
-    static uint REQUEST_VIDEO_OPEN;
-    static uint REQUEST_VIDEO_CLOSE;
+    /**
+     * video-stream control signal
+     */
+    // 开启视频流
+    static uchar REQUEST_VIDEO_OPEN;
+    static uchar REQUEST_VIDEO_CLOSE;
+
+    /**
+     * rov motion control signal
+     */
+    static uchar MOTION_FORWARD;
+    static uchar MOTION_BACK;
+    static uchar MOTION_UP;
+    static uchar MOTION_DOWN;
+    // 原地转弯
+    static uchar MOTION_ROUND;
+    // 速度调节
+    static uchar CONFIG_SPEED;
 };
 
 }   // namespace config
